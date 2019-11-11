36fa05d12d68dbd92acdde6815880f3382cee6ac
AsaOfDiamonds <asaofdiamonds@gmail.com>
2019-11-10 20:56:00 -0700

    ToDo.js ToDoItem.js configureStore.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import { configureStore } from "./redux/store/configureStore";
-import './index.css';
+import configureStore from "./redux/store/configureStore";
+//import './index.css';
 import App from './App';
 
 const store = configureStore();
